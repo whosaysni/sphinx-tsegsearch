5960e2a3024988c788c655c036de874c61fa92b0
hokorobi <hokorobi.hokorobi@gmail.com>
2018-08-24 18:06:23 +0900

    Support Sphinx 1.8

--- sphinx_tsegsearch/__init__.py
+++ sphinx_tsegsearch/__init__.py
@@ -2,18 +2,18 @@
 import os
 from sphinx.jinja2glue import SphinxFileSystemLoader
 
-def setup(app):
+def builder_inited(app):
+    extdir = os.path.dirname(os.path.abspath(__file__))
+    jsdir = os.path.join(extdir, 'static')
+    app.builder.config.html_static_path.append(jsdir)
 
-    def builder_inited(app_):
-        if app_.builder.name != 'html':
-            return
-        extdir = os.path.dirname(os.path.abspath(__file__))
-        templatedir = os.path.join(extdir, 'templates')
-        jsdir = os.path.join(extdir, 'static')
-        loader = app_.builder.templates
-        loader.pathchain.insert(0, templatedir)
-        loader.loaders.insert(0, SphinxFileSystemLoader(templatedir))
-        loader.templatepathlen += 1
-        app_.builder.config.html_static_path.append(jsdir)
-        app_.add_javascript('tiny_segmenter.js')
+    loader = app.builder.templates
+    templatedir = os.path.join(extdir, 'templates')
+    loader.pathchain.insert(0, templatedir)
+    loader.loaders.insert(0, SphinxFileSystemLoader(templatedir))
+    loader.templatepathlen += 1
+
+def setup(app):
     app.connect('builder-inited', builder_inited)
+    app.add_javascript('tiny_segmenter.js')
+

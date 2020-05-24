9c145114fe7d18a5bd0c6e00b043bdd644eb5cf7
hokorobi <hokorobi.hokorobi@gmail.com>
2020-05-24 13:16:38 +0900

    Assume no version_info

--- sphinx_tsegsearch/__init__.py
+++ sphinx_tsegsearch/__init__.py
@@ -16,8 +16,8 @@ def setup(app):
         loader.loaders.insert(0, SphinxFileSystemLoader(templatedir))
         loader.templatepathlen += 1
         app_.builder.config.html_static_path.append(jsdir)
-        if sphinx.version_info < (1, 8):
-            app_.add_javascript('tiny_segmenter.js')
-        else:
+        if 'version_info' in dir(sphinx) and sphinx.version_info >= (1, 8):
             app_.add_js_file('tiny_segmenter.js')
+        else:
+            app_.add_javascript('tiny_segmenter.js')
     app.connect('builder-inited', builder_inited)

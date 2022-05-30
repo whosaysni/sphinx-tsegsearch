b4fd086c29cac4d616580590ce1872c962f1a697
ronin-gw <andy@a-fal.co>
2022-05-30 19:10:57 +0900

    Use js_splitter_code to embed splitQuery

--- sphinx_tsegsearch/__init__.py
+++ sphinx_tsegsearch/__init__.py
@@ -3,6 +3,17 @@ import os
 
 import sphinx
 from sphinx.jinja2glue import SphinxFileSystemLoader
+from sphinx.search.ja import SearchJapanese
+
+_extdir = os.path.dirname(os.path.abspath(__file__))
+_jsdir = os.path.join(_extdir, 'static')
+
+with open(os.path.join(_jsdir, 'splitQuery.js')) as js:
+    js_splitter_code = js.read()
+
+
+class SearchJapaneseWithJSSplitter(SearchJapanese):
+    js_splitter_code = js_splitter_code
 
 
 def setup(app):
@@ -10,19 +21,23 @@ def setup(app):
         if app_.builder.name != 'html':
             return
 
-        extdir = os.path.dirname(os.path.abspath(__file__))
-        templatedir = os.path.join(extdir, 'templates')
-        jsdir = os.path.join(extdir, 'static')
-
-        loader = app_.builder.templates
-        loader.pathchain.insert(1, templatedir)
-        loader.loaders.insert(1, SphinxFileSystemLoader(templatedir))
-        loader.templatepathlen += 1
-
-        app_.builder.config.html_static_path.append(jsdir)
         if 'version_info' in dir(sphinx) and sphinx.version_info >= (1, 8):
-            app_.add_js_file('tiny_segmenter.js')
+            add_js_file = app_.add_js_file
         else:
-            app_.add_javascript('tiny_segmenter.js')
+            add_js_file = app_.add_javascript
+        app_.builder.config.html_static_path.append(_jsdir)
+        add_js_file('tiny_segmenter.js')
+
+        if 'version_info' in dir(sphinx) and sphinx.version_info >= (3, 0, 0):
+            app_.add_search_language(SearchJapaneseWithJSSplitter)
+        else:
+            templatedir = os.path.join(_extdir, 'templates')
+
+            loader = app_.builder.templates
+            loader.pathchain.insert(1, templatedir)
+            loader.loaders.insert(1, SphinxFileSystemLoader(templatedir))
+            loader.templatepathlen += 1
+
+            add_js_file('splitQuery.js')
 
     app.connect('builder-inited', builder_inited)
